13cf6e3691bfef5ee9c3df68abf7fd08e6826f68
unknown <cakahookele@697-334-Andunie.sf.k12.internal>
2015-11-02 11:38:35 -0800

    Should fix that japanese

--- Farm.java
+++ Farm.java
@@ -1,14 +1,14 @@
 class Farm 
 {     
-	private Animal[] animals = new Animal[3];
+	private Animal[] aBunchofAnimals = new Animal[3];
 	public Farm(){
-		animals[0] = new Cow("牛", "モーモー");
-		animals[1] = new Chick("ひよこ", "ぴよぴよ");
-		animals[2] = new Pig("ぶた", "ブーブー");
+		aBunchofAnimals[0] = new Cow("牛", "モーモー");
+		aBunchofAnimals[1] = new Chick("ひよこ", "ぴよぴよ");
+		aBunchofAnimals[2] = new Pig("ぶた", "ブーブー");
 	}
 	public void animalSounds(){
-		for (int nI = 0; nI < animals.length; nI++){
-			System.out.println(animals[nI].getType() + " goes " + animals[nI].getSound());
+		for (int nI = 0; nI < aBunchofAnimals.length; nI++){
+			System.out.println(aBunchofAnimals[nI].getType() + " goes " + aBunchofAnimals[nI].getSound());
 		}
 	}
    //your code here
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,13 +1,7 @@
-
-Farm [] yoFarm;
-
 void setup()
 {
-	yoFarm = new Farm [3];
-	for (int i = 0; i < yoFarm.length; i++) {
-		yoFarm[i] = new Farm();
-		System.out.println(yoFarm[i].getType() + " goes " + yoFarm[i].getSound());
-	}
+	Farm yoFarm = new Farm();
+	yoFarm.animalSounds();
 	//your variable declarations here
 }
 
