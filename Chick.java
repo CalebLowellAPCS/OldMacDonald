6ef81f2eac094bc1458be2896cf4925285637196
unknown <cakahookele@697-334-Andunie.sf.k12.internal>
2015-11-03 10:35:35 -0800

    Fix abstract

--- Chick.java
+++ Chick.java
@@ -2,14 +2,25 @@ class Chick implements Animal
 {     
      private String myType;
      private String mySound;
-     public Chick(String type, String sound){
+     private String mySound2;
+     public Chick(String type, String sound, String sound2){
      	myType = type;
      	mySound = sound;
+          mySound2 = sound2
      }
      public Chick(){
      	myType = "unknown";
      	mySound = "unknown";
+          mySound2 = "unknown";
      }
-     public String getSound(){return mySound;}
+
+     if (((int)Math.random()*10) => 5){
+          public String getSound(){return mySound;}
+     }
+     else{
+          public String getSound(){return mySound2;}
+
+     }
+
      public String getType(){return myType;}
 }
--- Farm.java
+++ Farm.java
@@ -2,9 +2,9 @@ class Farm
 {     
 	private Animal[] aBunchofAnimals = new Animal[3];
 	public Farm(){
-		aBunchofAnimals[0] = new Cow("牛", "モーモー");
-		aBunchofAnimals[1] = new Chick("ひよこ", "ぴよぴよ");
-		aBunchofAnimals[2] = new Pig("ぶた", "ブーブー");
+		aBunchofAnimals[0] = new Cow("Meat", "Galge");
+		aBunchofAnimals[1] = new Chick("Kobato", "Kukuku");
+		aBunchofAnimals[2] = new Pig("Yozora", "It's not like I'm desperate to have friends, anyway.");
 	}
 	public void animalSounds(){
 		for (int nI = 0; nI < aBunchofAnimals.length; nI++){
